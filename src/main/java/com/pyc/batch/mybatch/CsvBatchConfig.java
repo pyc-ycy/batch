63c327f3dc57f6c35a42478d44eb8ac1da3c3f3a
ycy-ycy <2923616405@qq.com>
2020-02-14 18:50:51 +0800

    Update

--- src/main/java/com/pyc/batch/mybatch/CsvBatchConfig.java
+++ src/main/java/com/pyc/batch/mybatch/CsvBatchConfig.java
@@ -7,5 +7,34 @@
 
 package com.pyc.batch.mybatch;
 
+import com.pyc.batch.domain.Person;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+@EnableBatchProcessing
 public class CsvBatchConfig {
+
+    @Bean
+    public ItemReader<Person> reader() throws Exception {
+        FlatFileItemReader<Person> reader = new FlatFileItemReader<>();
+        reader.setResource(new ClassPathResource("people.csv"));
+        reader.setLineMapper(new DefaultLineMapper<Person>() {
+            {
+                setLineTokenizer(new DelimitedLineTokenizer() {{
+                    setNames(new String[]{"name", "age", "nation", "address"});
+                }});
+                setFieldSetMapper(new BeanWrapperFieldSetMapper<Person>(){{
+                    setTargetType(Person.class);
+                }});
+            }});
+        return reader;
+    }
 }
