2fc5a524ce3a8ad860de2c494b09fc07589efdc3
ycy-ycy <2923616405@qq.com>
2020-02-14 18:43:52 +0800

    Update

---
+++ src/main/java/com/pyc/batch/mybatch/CsvBatchConfig.java
@@ -0,0 +1,11 @@
+//IntelliJ IDEA
+//batch
+//CsvBatchConfig
+//2020/2/14
+// Author:御承扬
+//E-mail:2923616405@qq.com
+
+package com.pyc.batch.mybatch;
+
+public class CsvBatchConfig {
+}
---
+++ src/main/java/com/pyc/batch/mybatch/CsvJobListener.java
@@ -0,0 +1,29 @@
+//IntelliJ IDEA
+//batch
+//CsvJobListener
+//2020/2/14
+// Author:御承扬
+//E-mail:2923616405@qq.com
+
+package com.pyc.batch.mybatch;
+
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
+
+public class CsvJobListener implements JobExecutionListener {
+    long startTime;
+    long endTime;
+
+    @Override
+    public void beforeJob(JobExecution jobExecution) {
+        startTime = System.currentTimeMillis();
+        System.out.println("任务处理开始");
+    }
+
+    @Override
+    public void afterJob(JobExecution jobExecution) {
+        endTime = System.currentTimeMillis();
+        System.out.println("任务处理结束");
+        System.out.println("耗时：" + (endTime - startTime) + "ms");
+    }
+}
